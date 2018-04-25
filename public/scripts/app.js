0e6acee1f85a21298e3594a7fff2a4984fcd5d61
thegreatsantini <lss13@my.fsu.edu>
2018-04-24 23:03:34 -0700

    mateirialize css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
-
-
 $(document).ready(function () {
+    $('.modal1').on('click', ()=> {
+        console.log('that tickles')
+    })
+    $('.modal1').modal();
 
-    
+    $('#get-started').on('click', ()=>{
+        $('#get-started').toggleClass('hide')
+        $('.ingredients').toggleClass('ingredients')
+        $('.ingredients').toggleClass('animation-slideframe')
+    })
 });

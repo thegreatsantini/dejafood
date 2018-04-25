c6dd05e2899632c6bb0d6a4f5174e7b9764ecf9b
thegreatsantini <lss13@my.fsu.edu>
2018-04-24 20:12:12 -0700

    changed api

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,52 @@ const express = require("express");
 const indexRoute = express.Router();
 const request = require('request');
 const bodyParser = require('body-parser');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 indexRoute.get("/", function (req, res) {
     res.render('index');
 });
 
-indexRoute.get('/ingredients', function(req, res){
-    let query = JSON.stringify(req.query.search)
-    
-    query = query.replace(/\s/g,'+').replace(/,/g,'%2C');
-    console.log(query)//this works
-    // 'https://api.edamam.com/search?app_id=' + process.env.API_ID + '&app_key=' + process.env.API_KEY + '&q=' + queryString + '&health=' + healthOption + '&to=100';
-    request(`https://api.edamam.com/search?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}&q=${query}&to=100`, function(error, response, body){
+// indexRoute.get('/ingredients', function(req, res){
+//     let query = JSON.stringify(req.query.search)
+
+//     query = query.replace(/\s/g,'+').replace(/,/g,'%2C');
+//     console.log(query)//this works
+//     // https://api.edamam.com/search?app_id=' + process.env.API_ID + '&app_key=' + process.env.API_KEY + '&r=' + recipeAPIUri
+//     // request(`https://api.edamam.com/search?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}&q=${query}&to=100`, function(error, response, body){
+//     //     if (error) {
+//     //         console.log('error')
+//     //     }
+//     //     // console.log("#########",body);
+//     //     parsedBody = JSON.parse(body)
+//     //     console.log(parsedBody)
+//     // })
+// })
+
+indexRoute.get('/ingredients', function (req, res) {
+
+    let queryArr = []
+    let userQuery;
+
+
+
+    userQueryArr = req.query.search.split(',');
+    // console.log('userQueryArr:', userQueryArr)
+    userQuery = userQueryArr.join();
+    // userQuery = userQuery.replace(/\s/g,'')
+    // console.log('userQuery:', userQuery)
+
+    const recipeUrl = `http://food2fork.com/api/search?key=${process.env.API_KEY}&q=${userQuery}`;
+
+    request(recipeUrl, function (error, response, body) {
         if (error) {
-            console.log('error')
+            console.log('***************error***********', error)
         }
-        // console.log("#########",body);//why are you doing this to me
-        // parsedBody = JSON.parse(body)
-        // console.log(parsedBody)
-        res.send(body);
-    })
+        console.log(body)
+        // const recipe = JSON.parse(body);
+        // console.log(recipe)
+        // res.json(recipe);
+    });
 })
 
 module.exports = indexRoute;
